b7cdc92d94e77c7f7e50d3e1057fa5fed5662661
SuyeongOh <pental_vega@naver.com>
2025-02-17 15:57:27 +0900

    remove firebase service

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:vitalsync_dashboard/ui/page/page_home.dart';
 import 'package:vitalsync_dashboard/ui/page/page_login.dart';
 import 'package:vitalsync_dashboard/ui/page/page_userlist.dart';
-import 'package:vitalsync_dashboard/services/auth_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Firebase 초기화
   runApp(const VitalSyncDashBoard());
 }
 
@@ -18,7 +16,6 @@ class VitalSyncDashBoard extends StatelessWidget {
     return MaterialApp(
       title: 'Vital Sync Dashboard',
       debugShowCheckedModeBanner: false,
-      initialRoute: AuthService().isLoggedIn() ? '/home' : '/login',
       routes: {
         '/login': (context) => LoginPage(),
         '/home': (context) => HomePage(),
